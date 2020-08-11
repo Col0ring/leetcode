c2c7211e23ad6eb96eebdce8336315d6341b7287
Col0ring <1561999073@qq.com>
2020-08-11 19:06:50 +0800

    feat: 新增双端队列解法

--- 数据结构/数组和字符串/字符串简介/03-翻转字符串里的单词.ts
+++ 数据结构/数组和字符串/字符串简介/03-翻转字符串里的单词.ts
@@ -1,3 +1,21 @@
+// function reverseWords(s: string): string {
+//   //   return s.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
+//   return s.trim().split(/\s+/).reverse().join(' ')
+// }
+
+// 双端队列
 function reverseWords(s: string): string {
-  return s.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
+  s = s.trim()
+  let ans: string[] = []
+  let word = ''
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== ' ') {
+      word += s[i]
+    } else if (s[i + 1] !== ' ') {
+      ans.unshift(word)
+      word = ''
+    }
+  }
+  ans.unshift(word)
+  return ans.join(' ')
 }
