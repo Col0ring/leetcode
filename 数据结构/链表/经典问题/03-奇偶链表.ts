891a9f85320f732fd0ef64e444db07e4df850867
Col0ring <1561999073@qq.com>
2020-08-14 17:33:37 +0800

    feat: 新增数据结构-链表-经典问题

---
+++ 数据结构/链表/经典问题/03-奇偶链表.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export function oddEvenList(head: ListNode | null): ListNode | null {
+  if (head == null) return null
+  let odd = head
+  let even = head.next
+  const evenHead = even
+  while (even && even.next) {
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+  return head
+}
