5c7659e8e39be0ec9f5101c16cf8155ed7fe86ed
Col0ring <1561999073@qq.com>
2020-09-01 15:35:47 +0800

    feat: 新增算法-二分查找-背景

---
+++ 算法/二分查找/背景/01-二分查找.ts
@@ -0,0 +1,22 @@
+// // api
+// export function search(nums: number[], target: number): number {
+//   return nums.indexOf(target)
+// }
+
+// 二分
+export function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    const mid = Math.floor((right + left) / 2)
+    if (nums[mid] == target) {
+      return mid
+    }
+    if (target < nums[mid]) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return -1
+}
