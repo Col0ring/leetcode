c44b83ff3faae2b895d4c83e5e8ef8583a34a147
Col0ring <1561999073@qq.com>
2020-10-08 16:08:10 +0800

    feat: 新增每日一题

---
+++ 每日一题/20-10/344. 反转字符串.ts
@@ -0,0 +1,26 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+// function reverseString(s: string[]): void {
+//   s.reverse()
+// }
+
+// export function reverseString(s: string[]): void {
+//   for (let i = 0; i < Math.floor(s.length / 2); i++) {
+//     const temp = s[i]
+//     s[i] = s[s.length - i - 1]
+//     s[s.length - i - 1] = temp
+//   }
+// }
+
+// 递归
+export function reverseString(s: string[]): void {
+  const helper = (s: string[], left: number, right: number): void => {
+    if (left >= right) return
+    const tmp = s[left]
+    s[left++] = s[right]
+    s[right--] = tmp
+    helper(s, left, right)
+  }
+  helper(s, 0, s.length - 1)
+}
