7c10b590b38681892904a70721f329bf8cbe5ad3
Col0ring <1561999073@qq.com>
2020-11-28 23:49:16 +0800

    feat: 新增每日一题

---
+++ 每日一题/20-11/493. 翻转对.ts
@@ -0,0 +1,53 @@
+export function reversePairs(nums: number[]): number {
+  const reversePairsRecursive = (
+    nums: number[],
+    left: number,
+    right: number
+  ): number => {
+    if (left === right) {
+      return 0
+    } else {
+      const mid = Math.floor((left + right) / 2)
+      const n1 = reversePairsRecursive(nums, left, mid)
+      const n2 = reversePairsRecursive(nums, mid + 1, right)
+      let ret = n1 + n2
+
+      let i = left
+      let j = mid + 1
+      while (i <= mid) {
+        while (j <= right && nums[i] > 2 * nums[j]) {
+          j++
+        }
+        ret += j - mid - 1
+        i++
+      }
+
+      const sorted = new Array(right - left + 1)
+      let p1 = left,
+        p2 = mid + 1
+      let p = 0
+      while (p1 <= mid || p2 <= right) {
+        if (p1 > mid) {
+          sorted[p++] = nums[p2++]
+        } else if (p2 > right) {
+          sorted[p++] = nums[p1++]
+        } else {
+          if (nums[p1] < nums[p2]) {
+            sorted[p++] = nums[p1++]
+          } else {
+            sorted[p++] = nums[p2++]
+          }
+        }
+      }
+      for (let k = 0; k < sorted.length; k++) {
+        nums[left + k] = sorted[k]
+      }
+      return ret
+    }
+  }
+
+  if (nums.length === 0) {
+    return 0
+  }
+  return reversePairsRecursive(nums, 0, nums.length - 1)
+}
