94a49295a5240fec3d3ebab88b5ca7e12bf65cbe
Col0ring <1561999073@qq.com>
2020-12-19 23:55:08 +0800

    feat: 新增每日一题

---
+++ 每日一题/20-12/48. 旋转图像.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+export function rotate(matrix: number[][]): void {
+  const n = matrix.length
+  const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0))
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix_new[j][n - i - 1] = matrix[i][j]
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix[i][j] = matrix_new[i][j]
+    }
+  }
+}
