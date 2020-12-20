d5f88c6c197856030bd798f8ae67b86b70b93903
Col0ring <1561999073@qq.com>
2020-12-20 23:56:31 +0800

    feat: 新增每日一题

--- 每日一题/20-12/48. 旋转图像.ts
+++ 每日一题/20-12/48. 旋转图像.ts
@@ -15,3 +15,18 @@ export function rotate(matrix: number[][]): void {
     }
   }
 }
+// /**
+//  Do not return anything, modify matrix in-place instead.
+//  */
+// // 本质还是找规律
+// export function rotate(matrix: number[][]): void {
+//     const n = matrix.length
+//     for (let i = 0; i < n; i++) {
+//       for (let j = i; j < n; j++) {
+//         const temp = matrix[i][j]
+//         matrix[i][j] = matrix[j][i]
+//         matrix[j][i] = temp
+//       }
+//     }
+//     matrix.forEach((row) => row.reverse())
+//   }
