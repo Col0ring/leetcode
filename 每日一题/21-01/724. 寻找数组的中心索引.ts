74ad5befb1e931943b6c46ad1bc83bdd83e9eeec
Col0ring <1561999073@qq.com>
2021-01-28 23:46:22 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-01/724. 寻找数组的中心索引.ts
@@ -0,0 +1,11 @@
+export function pivotIndex(nums: number[]): number {
+  const total = nums.reduce((a, b) => a + b, 0)
+  let sum = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (2 * sum + nums[i] === total) {
+      return i
+    }
+    sum += nums[i]
+  }
+  return -1
+}
