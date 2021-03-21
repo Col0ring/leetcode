4c0e922b9af8311f6802c23ac57c19d90d9bdd93
Col0ring <1561999073@qq.com>
2021-03-21 23:34:24 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-03/73. 矩阵置零.ts
@@ -0,0 +1,23 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+export function setZeroes(matrix: number[][]): void {
+  const m = matrix.length,
+    n = matrix[0].length
+  const row: boolean[] = new Array(m).fill(false)
+  const col: boolean[] = new Array(n).fill(false)
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        row[i] = col[j] = true
+      }
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (row[i] || col[j]) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+}
