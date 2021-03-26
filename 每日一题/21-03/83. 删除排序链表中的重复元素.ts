be4d4e7f1c2f3b3e6d96d605cab29713ec6c28c8
Col0ring <1561999073@qq.com>
2021-03-26 23:51:33 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-03/83. 删除排序链表中的重复元素.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return head
+  }
+
+  let cur = head
+  while (cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return head
+}
