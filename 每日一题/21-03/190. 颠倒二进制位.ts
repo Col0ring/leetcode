cb2a56f9f24c474edf927f187b6f4eb250a32cd2
Col0ring <1561999073@qq.com>
2021-03-29 23:18:15 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-03/190. 颠倒二进制位.ts
@@ -0,0 +1,10 @@
+// 逐位颠倒
+
+export function reverseBits(n: number): number {
+  let rev = 0
+  for (let i = 0; i < 32 && n > 0; ++i) {
+    rev |= (n & 1) << (31 - i)
+    n >>>= 1
+  }
+  return rev >>> 0
+}
