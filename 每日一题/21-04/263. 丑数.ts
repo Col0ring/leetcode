81c95eb0d8894052307a2f1b29469f88c77c3fbc
Col0ring <1561999073@qq.com>
2021-04-10 23:31:26 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-04/263. 丑数.ts
@@ -0,0 +1,12 @@
+export function isUgly(n: number): boolean {
+  if (n <= 0) {
+    return false
+  }
+  const factors = [2, 3, 5]
+  for (const factor of factors) {
+    while (n % factor === 0) {
+      n /= factor
+    }
+  }
+  return n == 1
+}
