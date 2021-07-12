978f35d1ed649b2e96f066bfeac49d6b47519538
Col0ring <1561999073@qq.com>
2021-07-12 23:48:39 +0800

    feat: 新增每日一题

---
+++ 每日一题/20-07/275. H 指数 II.ts
@@ -0,0 +1,15 @@
+// 二分
+export function hIndex(citations: number[]): number {
+  let n = citations.length
+  let left = 0,
+    right = n - 1
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2)
+    if (citations[mid] >= n - mid) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return n - left
+}
