7a5f67b37f197ec4e51a20e3742098caecbc894f
Col0ring <1561999073@qq.com>
2021-09-07 09:42:04 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-09/704. 二分查找.ts
@@ -0,0 +1,15 @@
+export function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2)
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] > target) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return -1
+}
