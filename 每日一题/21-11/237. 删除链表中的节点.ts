fc7d7f737aee1a190ab621a084bedc9e037fc877
Col0ring <1561999073@qq.com>
2021-11-02 22:05:14 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-11/237. 删除链表中的节点.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+export function deleteNode(root: ListNode | null): void {
+  if (!root) {
+    return
+  }
+  root.val = root.next!.val
+  root.next = root.next!.next
+}
