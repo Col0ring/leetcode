27bd112ef8b445bd63516b3ed8a2b6f347a39443
Col0ring <1561999073@qq.com>
2021-11-26 23:43:55 +0800

    feat: 新增每日一题

---
+++ 每日一题/21-11/700. 二叉搜索树中的搜索.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.left = left === undefined ? null : left
+    this.right = right === undefined ? null : right
+  }
+}
+
+export function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  while (root) {
+    if (root.val === val) {
+      return root
+    } else if (root.val > val) {
+      root = root.left
+    } else if (root.val < val) {
+      root = root.right
+    }
+  }
+  return null
+}
