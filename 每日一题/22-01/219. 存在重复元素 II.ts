15361440d78bb65ee1fc196d7dcef3f91b773cc5
Col0ring <1561999073@qq.com>
2022-01-23 23:57:01 +0800

    feat: 新增每日一题

--- 每日一题/22-01/219. 存在重复元素 II.ts
+++ 每日一题/22-01/219. 存在重复元素 II.ts
@@ -1,6 +1,6 @@
 export function containsNearbyDuplicate(nums: number[], k: number): boolean {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; i++) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] === nums[j] && j - i <= k) {
         return true
       }
