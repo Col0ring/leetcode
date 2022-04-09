32cb68d9b050dab6d3a4596d641bbe7a47f3aa10
Col0ring <1561999073@qq.com>
2022-04-09 23:56:47 +0800

    feat: 新增每日一题

---
+++ 每日一题/22-04/780. 到达终点.ts
@@ -0,0 +1,23 @@
+export function reachingPoints(
+  sx: number,
+  sy: number,
+  tx: number,
+  ty: number
+): boolean {
+  while (tx > sx && ty > sy && tx != ty) {
+    if (tx > ty) {
+      tx %= ty
+    } else {
+      ty %= tx
+    }
+  }
+  if (tx === sx && ty === sy) {
+    return true
+  } else if (tx === sx) {
+    return ty > sy && (ty - sy) % tx === 0
+  } else if (ty === sy) {
+    return tx > sx && (tx - sx) % ty === 0
+  } else {
+    return false
+  }
+}
