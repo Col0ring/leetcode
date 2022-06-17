31b9a7bd362e3d52933fcce3c4c9568bb218fbd0
Col0ring <1561999073@qq.com>
2022-06-17 23:32:00 +0800

    feat: 新增每日一题

---
+++ 每日一题/22-06/1089. 复写零.ts
@@ -0,0 +1,31 @@
+/**
+ Do not return anything, modify arr in-place instead.
+ */
+export function duplicateZeros(arr: number[]): void {
+  const n = arr.length
+  let top = 0
+  let i = -1
+  while (top < n) {
+    i++
+    if (arr[i] !== 0) {
+      top++
+    } else {
+      top += 2
+    }
+  }
+  let j = n - 1
+  if (top === n + 1) {
+    arr[j] = 0
+    j--
+    i--
+  }
+  while (j >= 0) {
+    arr[j] = arr[i]
+    j--
+    if (arr[i] === 0) {
+      arr[j] = arr[i]
+      j--
+    }
+    i--
+  }
+}
