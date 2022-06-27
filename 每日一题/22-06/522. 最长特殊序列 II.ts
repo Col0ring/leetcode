73ea17433bbdf3c0e8af41bffd8d69eae15e2538
Col0ring <1561999073@qq.com>
2022-06-27 23:47:41 +0800

    feat: 新增每日一题

---
+++ 每日一题/22-06/522. 最长特殊序列 II.ts
@@ -0,0 +1,29 @@
+export function findLUSlength(strs: string[]): number {
+  const n = strs.length
+  let ans = -1
+  for (let i = 0; i < n; ++i) {
+    let check = true
+    for (let j = 0; j < n; ++j) {
+      if (i !== j && isSubseq(strs[i], strs[j])) {
+        check = false
+        break
+      }
+    }
+    if (check) {
+      ans = Math.max(ans, strs[i].length)
+    }
+  }
+  return ans
+}
+
+const isSubseq = (s: string, t: string) => {
+  let ptS = 0,
+    ptT = 0
+  while (ptS < s.length && ptT < t.length) {
+    if (s[ptS] === t[ptT]) {
+      ++ptS
+    }
+    ++ptT
+  }
+  return ptS === s.length
+}
