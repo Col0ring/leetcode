13c2cfd9d4481137f4a16e264b808313b0a11ecc
Col0ring <1561999073@qq.com>
2022-06-28 23:44:25 +0800

    feat: 新增每日一题

---
+++ 每日一题/22-06/324. 摆动排序 II.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+export function wiggleSort(nums: number[]): void {
+  const arr = nums.slice()
+  arr.sort((a, b) => a - b)
+  const n = nums.length
+  const x = Math.floor((n + 1) / 2)
+  for (let i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--) {
+    nums[i] = arr[j]
+    if (i + 1 < n) {
+      nums[i + 1] = arr[k]
+    }
+  }
+}
